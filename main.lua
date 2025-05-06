0518340359737b3e395846ceea2bd34bdb97c2a3
ryleyw8412 <71460861+ryleyw8412@users.noreply.github.com>
2025-05-06 19:14:50 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@
 local G2L = {};
 
 -- StarterGui.Faces_Gui
-G2L["1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
+G2L["1"] = Instance.new("ScreenGui", game.CoreGui);
 G2L["1"]["Name"] = [[Faces_Gui]];
 G2L["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
 
